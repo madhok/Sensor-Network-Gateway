0fe828d6b8fa041294592868ccfa15a66b70478e
Madhok Shivaratre <gsmadhok@gmail.com>
2013-03-16 09:49:04 -0700

    New Building Map

--- gen/com/sensorgateway/R.java
+++ gen/com/sensorgateway/R.java
@@ -19,9 +19,10 @@ public final class R {
     }
     public static final class drawable {
         public static final int first_floor=0x7f020000;
-        public static final int ic_action_search=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int second_floor=0x7f020003;
+        public static final int first_floor_old=0x7f020001;
+        public static final int ic_action_search=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int second_floor=0x7f020004;
     }
     public static final class id {
         public static final int GraphViewlay=0x7f090011;
